111867317b772e2a6c97f15dba0fe0a12d17b4d3
kaiqui <kaiqui82@gmail.com>
2024-07-19 15:53:09 -0300

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="kaiqui",
     author_email="kaiqui82@gmail.com",
     description="Simple security middleware for FastAPI",
-    version="0.1.2",
+    version="0.1.3",
     setup_requires=['setuptools_scm'],
     long_description=long_description,
     long_description_content_type="text/markdown",
